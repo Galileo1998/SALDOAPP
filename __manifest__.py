f2d5ebaf333751fc2b05261c5eea66620c7062f3
Galileo1998 <galileoga63@gmail.com>
2021-02-12 15:46:11 -0800

    Views, models

--- __manifest__.py
+++ __manifest__.py
@@ -3,7 +3,7 @@
     "description": "Aplicaciòn para registrar ingresos y egresos",
     "author": "Galileo Garcìa",
     "version": "1.0",
-    "depends": ["base"],
+    "depends": ["base", "mail"],
     "data": [
         "security/ir_model_access.xml",
         "views/views.xml"
--- models/models.py
+++ models/models.py
@@ -3,20 +3,36 @@ from odoo import fields, models
 class Movimiento(models.Model):
     _name = "sa.movimiento"
     _description = "Movimiento"
+    _inherit = "mail.thread"
 
-    name = fields.Char("Nombre")
-    type_move = fields.Selection(selection=[("ingreso", "Ingreso"), ("gasto", "Gasto")], string="Tipo de movimiento")
-    date = fields.Datetime("Fecha")
-    amount = fields.Float("Monto")
+    name = fields.Char(string="Nombre", required=True)
+    type_move = fields.Selection(selection=[("ingreso", "Ingreso"), ("gasto", "Gasto")], string="Tipo de movimiento", default="ingreso", required=True)
+    date = fields.Datetime(string="Fecha")
+    amount = fields.Float("Monto", track_visibility="onchange")
     receipt_image = fields.Binary("Foto del recibo")
+    notas = fields.Html("Notas")
+
+    currency_id = fields.Many2one("res.currency", default=44)
 
     user_id = fields.Many2one("res.users", string="Usuario")
     category_id = fields.Many2one("sa.category", "Categoria")
+    tag_id = fields.Many2many("sa.tag", "sa_mov_sa_tag_rel", "move_id", "tag_id")
+
 class Category(models.Model):
     _name = "sa.category"
     _description = "Categoria"
 
     name = fields.Char("Nombre")
+
+    def ver_movimientos(self):
+        return {
+            "type": "ir.actions.act_window",
+            "name":"Movimientos de categoria: "+self.name,
+            "res_model": "sa.movimiento",
+            "views": [[False, "tree"]],
+            "target": "new",
+            "domain": [["category_id", "=", self.id]]
+        }
 
 class Tag(models.Model):
     _name = "sa.tag"
