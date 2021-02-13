f8935c48caba053998ca5d3c4fdb7c9cd0e68afc
Galileo1998 <galileoga63@gmail.com>
2021-02-13 12:54:40 -0800

    Vistas avanzadas

--- models/models.py
+++ models/models.py
@@ -1,4 +1,5 @@
-from odoo import fields, models
+from odoo import fields, models, api
+from odoo.exceptions import ValidationError
 
 class Movimiento(models.Model):
     _name = "sa.movimiento"
@@ -6,7 +7,7 @@ class Movimiento(models.Model):
     _inherit = "mail.thread"
 
     name = fields.Char(string="Nombre", required=True)
-    type_move = fields.Selection(selection=[("ingreso", "Ingreso"), ("gasto", "Gasto")], string="Tipo de movimiento", default="ingreso", required=True)
+    type_move = fields.Selection(selection=[("ingreso", "Ingreso"), ("gasto", "Gasto")], string="Tipo de movimiento", default="ingreso", required=True, track_visibility="onchange")
     date = fields.Datetime(string="Fecha")
     amount = fields.Float("Monto", track_visibility="onchange")
     receipt_image = fields.Binary("Foto del recibo")
@@ -14,15 +15,52 @@ class Movimiento(models.Model):
 
     currency_id = fields.Many2one("res.currency", default=44)
 
-    user_id = fields.Many2one("res.users", string="Usuario")
+    user_id = fields.Many2one("res.users", string="Usuario", default=lambda self: self.env.user.id)
     category_id = fields.Many2one("sa.category", "Categoria")
     tag_id = fields.Many2many("sa.tag", "sa_mov_sa_tag_rel", "move_id", "tag_id")
+
+    email = fields.Char(related="user_id.email", string="Correo electrònico")
+
+    @api.constrains("amount")
+    def _check_amount(self):
+        if not(self.amount>=0 and self.amount<=100000):
+            raise ValidationError("El monto debe encontrarse entre 0 y 100000")
+
+    @api.onchange("type_move")
+    def onchange_type_move(self):
+        if(self.type_move == "ingreso"):
+            self.name = "Ingreso: "
+        elif self.type_move == "gasto":
+            self.name = "Egreso"
+
+    @api.model
+    def create(self, vals):
+        name = vals.get("name", "-")
+        amount = vals.get("amount", "0")
+        type_move = vals.get("type_move", "")
+        date = vals.get("date", "")
+
+        notas = """ 
+            <p>Tipo de movimiento: {}</p>
+            <p>Nombre: {}</p>
+            <p>Monto: {}</p>
+            <p>Fecha: {}</p>
+        """
+        vals["notas"] = notas.format(type_move, name, amount, date)
+        return super(Movimiento, self).create(vals)
+
+    def unlink(self):
+        for record in self:
+            if record.amount >= 50:
+                raise ValidationError("Movimientos con montos mayores a 50 no pueden ser eliminados")
+        return super(Movimiento, self).unlink()
 
 class Category(models.Model):
     _name = "sa.category"
     _description = "Categoria"
 
     name = fields.Char("Nombre")
+    type_move = fields.Selection(selection=[("ingreso", "Ingreso"), ("gasto", "Gasto")], string="Tipo de movimiento", default="ingreso", required=True)
 
     def ver_movimientos(self):
         return {
@@ -39,8 +77,27 @@ class Tag(models.Model):
     _description = "Tag"
 
     name = fields.Char("Nombre")
+    type_move = fields.Selection(selection=[("ingreso", "Ingreso"), ("gasto", "Gasto")], string="Tipo de movimiento", default="ingreso", required=True)
 
 class ResUsers(models.Model):
     _inherit ="res.users"
 
     movimiento_ids = fields.One2many("sa.movimiento", "user_id")
+    total_ingresos = fields.Float("Total de Ingresos", compute="_compute_movimientos")
+    total_egresos = fields.Float("Total de Egresos", compute="_compute_movimientos")
+
+    @api.depends("movimiento_ids")
+    def _compute_movimientos(self):
+        for record in self:
+            record.total_ingresos = sum(record.movimiento_ids.filtered(lambda r: r.type_move == 'ingreso').mapped("amount"))
+            record.total_egresos = sum(record.movimiento_ids.filtered(lambda r: r.type_move == 'gasto').mapped("amount"))
+
+    def mi_cuenta(self):
+        return{
+            "type":"ir.actions.act_window",
+            "name":"Mi cuenta",
+            "res_model": "res.users",
+            "res_id": self.env.user.id,
+            "target": "self",
+            "views": [(False, "form")]
+        }
